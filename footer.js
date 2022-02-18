3c51b977c9644b93bf25f803af1767e09b590ffa
magejiaaa <dses60337@gmail.com>
2022-02-18 21:42:40 +0800

    細修微調0218

--- footer.js
+++ footer.js
@@ -9,7 +9,9 @@ footer = '<div class="container-fluid py-5 mx-auto text-center bg-light">\
         <h4 class="fw-bold">關於這東西</h4>\
         <p class="mb-0">插件作者詳見Github。<br>\
         又懶又不想動腦裝了插件，順便練習上課的內容做了個網站，再順便筆記插件的功能！<br>\
-        有問題可以DC<i class="fa-brands fa-discord"></i>秘境MJ#2897，或遊戲私訊 Big Yo(Gaia) ，翻譯錯誤也歡迎回報，很多機翻怕會錯意！</p>\
+        有問題可以DC\
+        <i class="bi bi-discord"></i>\
+        秘境MJ#2897，或遊戲私訊 Big Yo(Gaia) ，翻譯錯誤也歡迎回報，很多機翻怕會錯意！</p>\
     </div>\
 </div>\
 </div>';
