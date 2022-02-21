7bdd0e6c262d58b77c4dde08dfb3005c94eb5400
magejiaaa <dses60337@gmail.com>
2022-02-22 00:52:22 +0800

    更新首頁，增加貼心複製小功能

--- js/all.js
+++ js/all.js
@@ -12,3 +12,16 @@ $(document).ready(function () {
         }
     });
 });
+
+    // 剪貼簿
+    function myFunction() {
+        /* Get the text field */
+        var copyText = document.getElementById("myInput");
+      
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+      
+         /* Copy the text inside the text field */
+        navigator.clipboard.writeText(copyText.value);
+      }
