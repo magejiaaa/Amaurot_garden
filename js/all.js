a4051fab186770bca7f9fac9c9e17219e89c6f2e
magejiaaa <dses60337@gmail.com>
2022-03-04 17:33:42 +0800

    tweaks修改版面

--- js/all.js
+++ js/all.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         mouseenter: function () {
             //stuff to do on mouse enter
             $(this).removeClass("imagebigback")
-            $(this).attr("max-height","100%")
+            $(this).attr("max-height", "100%")
         },
         mouseleave: function () {
             //stuff to do on mouse leave
@@ -11,17 +11,24 @@ $(document).ready(function () {
             $(this).addClass("imagebigback")
         }
     });
-});
+
+    window.onload = function () {
+        var tableLine = document.getElementById("number");
+        for (var i = 0; i < tableLine.rows.length; i++) {
+            tableLine.rows[i].cells[0].innerHTML = (i + 1);
+        }
+    }
 
     // 剪貼簿
     function myFunction() {
         /* Get the text field */
         var copyText = document.getElementById("myInput");
-      
+
         /* Select the text field */
         copyText.select();
         copyText.setSelectionRange(0, 99999); /* For mobile devices */
-      
-         /* Copy the text inside the text field */
+
+        /* Copy the text inside the text field */
         navigator.clipboard.writeText(copyText.value);
-      }
+    }
+}
