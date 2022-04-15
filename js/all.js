f5692ced4e9794321894cfc7c31d3bf9ed984b8d
magejiaaa <dses60337@gmail.com>
2022-04-15 19:29:33 +0800

    修複製bug

--- js/all.js
+++ js/all.js
@@ -54,3 +54,15 @@ $(document).ready(function () {
         /* Copy the text inside the text field */
         navigator.clipboard.writeText(copyText.value);
     }
+
+    function myFunction2() {
+        /* Get the text field */
+        var copyText = document.getElementById("myInput2");
+
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+        /* Copy the text inside the text field */
+        navigator.clipboard.writeText(copyText.value);
+    }
