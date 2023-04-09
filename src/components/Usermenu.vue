f0384bcd5d7874715fc59c049842424e43aeb413
magejiaaa <dses60337@gmail.com>
2023-04-09 10:40:51 +0800

    讓選單不要亂跳

--- src/components/Usermenu.vue
+++ src/components/Usermenu.vue
@@ -1,6 +1,5 @@
 <template>
-    <nav
-        class="fixed top-0 right-0 w-full flex flex-wrap justify-between items-center bg-white h-20 z-10 | tooTop">
+    <nav class="fixed top-0 right-0 w-full flex flex-wrap justify-between items-center bg-white h-20 z-10 | tooTop">
         <router-link to="/" class="flex items-center p-4">
             <img src="../assets/images/logo.png" alt="" class="w-16 mr-3 h-auto">
             <h1 class="text-black font-light text-xl">亞馬烏羅提後花園</h1>
@@ -9,7 +8,8 @@
 
         <!-- 手機版漢堡 -->
         <button class="block w-8 h-8 mr-4
-                lg:hidden | mobileMenu" @click="mobileMenuShow = !mobileMenuShow" :class="{ 'is-opened': mobileMenuShow }">
+                    lg:hidden | mobileMenu" @click="mobileMenuShow = !mobileMenuShow"
+            :class="{ 'is-opened': mobileMenuShow }">
             <span></span>
             <span></span>
             <span></span>
@@ -20,18 +20,20 @@
             leave-active-class="transition duration-200 ease-in origin-top"
             leave-from-class="transform scale-y-100 opacity-100" leave-to-class="transform scale-y-0 opacity-0">
             <div class="w-full absolute top-20 
-                    lg:static lg:w-auto" v-if="mobileMenuShow">
+                        lg:static lg:w-auto" v-if="mobileMenuShow">
                 <!-- 選單按鈕 -->
                 <ul class="flex flex-col bg-white text-center pb-4
-                        lg:mr-4 lg:flex-row lg:pb-0 | menulist">
+                            lg:mr-4 lg:flex-row lg:pb-0 | menulist">
                     <li>
                         <router-link to="/" class="p-4 block" @click="mobileMenuClose">Dalamud介紹</router-link>
                     </li>
                     <li>
-                        <router-link to="/dashboard/pluginsList" class="p-4 block" @click="mobileMenuClose">插件介紹</router-link>
+                        <router-link to="/dashboard/pluginsList" class="p-4 block"
+                            @click="mobileMenuClose">插件介紹</router-link>
                     </li>
                     <li>
-                        <span class="p-4 block cursor-not-allowed text-gray-500" data-bs-toggle="page" @click="mobileMenuClose">第三方插件</span>
+                        <span class="p-4 block cursor-not-allowed text-gray-500" data-bs-toggle="page"
+                            @click="mobileMenuClose">第三方插件</span>
                     </li>
                     <li>
                         <a class="p-4 block cursor-pointer" @click.prevent="getUserID" v-if="isLogin">個人資料</a>
@@ -85,7 +87,9 @@ export default {
 
         // 選單點擊後關閉
         function mobileMenuClose() {
-            mobileMenuShow.value = false;
+            if (window.innerWidth < 1024) {
+                mobileMenuShow.value = false;
+            }
         }
 
         function logOut() {
